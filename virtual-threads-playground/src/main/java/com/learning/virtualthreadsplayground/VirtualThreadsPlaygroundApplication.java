62961068d5dfd9d1707c2ebc7e76b831623b3ced
shubham88fru <shubham88fru@yahoo.com>
2025-09-12 21:13:57 -0500

    init project

---
+++ virtual-threads-playground/src/main/java/com/learning/virtualthreadsplayground/VirtualThreadsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.learning.virtualthreadsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualThreadsPlaygroundApplication.class, args);
+    }
+
+}
